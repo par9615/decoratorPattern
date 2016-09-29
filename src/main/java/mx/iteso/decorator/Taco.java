18359b4598a05c3f2a91efc640b43059df60cd0d
Kireck211 <erickdesantiago11@gmail.com>
2016-09-28 21:13:05 -0500

    Adding the size property to the tacos

--- src/main/java/mx/iteso/decorator/Taco.java
+++ src/main/java/mx/iteso/decorator/Taco.java
@@ -1,9 +1,16 @@
 package mx.iteso.decorator;
 
 public abstract class Taco {
+
+    public static int MINI = 0;
+    public static int REGULAR = 1;
+    public static int MEGA = 2;
+
     public String description = "Any Taco";
     public String getDescription(){
         return description;
     }
     public abstract double cost();
+    public abstract void setStize(int size);
+    public abstract int getSize();
 }
